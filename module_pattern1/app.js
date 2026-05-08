477eccc79ef0a77ce77c0060ec4f6d187c909c03
rohith <gunnamrohith@gmail.com>
2026-05-08 12:48:41 +0530

    Add new FEDF tasks and project updates

---
+++ module_pattern1/app.js
@@ -0,0 +1,17 @@
+import { getProductById } from './ProductServices.js';
+import { addToCart, getCart } from './CartServices.js';
+import { calculateTotal } from './utils.js';
+
+// Add items
+const product1 = getProductById(1);
+const product2 = getProductById(2);
+
+addToCart(product1);
+addToCart(product2);
+
+// Show cart
+const cart = getCart();
+console.log("Cart:", cart);
+
+// Total price
+console.log("Total:", calculateTotal(cart));
